cb3a9dd949c0afbab5a38869306c7110942e908e
graham_bates <mogmog@gmail.com>
2020-10-11 12:41:00 +0100

    remove flags

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from urllib.parse import unquote
 async def abar(name):
 
   text='test'
-  browser = await launch()
+  browser = await launch(headless=True, args=['--no-sandbox'])
   page = await browser.newPage()
   await page.goto('https://www.calligrapher.ai/')
   await page.waitFor(1000)
